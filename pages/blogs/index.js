dce653a4251b55edb0276f038bb93eea317ce61d
zartaj <zartaj.afsar@gmail.com>
2022-12-09 18:17:34 +0530

    updated router

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -1,14 +1,13 @@
 // import '../../styles/globals.css'
 // import Navbar from '../components/NavBar'
 // import Footer from '../components/Footer'
-import SingleBlog from '../components/singleBlog.js'
+import SingleBlog from '../../components/singleBlog.js'
 
-function MyApp({ Component, pageProps }) {
+export default function Home() {
   return (
-    <>
+  
       <SingleBlog/>
-    </>
+    
   )
 }
 
-export default MyApp
